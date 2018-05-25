c645382379f3efdb2367f01778bb65dc0d453111
Icer <liangshuangde@163.com>
2018-05-25 09:15:31 +0800

    initial

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbCriteria = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.cmbCriteria);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSearch);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(530, 450);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 38);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cmbCriteria
+            // 
+            this.cmbCriteria.FormattingEnabled = true;
+            this.cmbCriteria.Items.AddRange(new object[] {
+            "互联网科技",
+            "网络科技"});
+            this.cmbCriteria.Location = new System.Drawing.Point(12, 12);
+            this.cmbCriteria.Name = "cmbCriteria";
+            this.cmbCriteria.Size = new System.Drawing.Size(238, 21);
+            this.cmbCriteria.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox cmbCriteria;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +19,76 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string searchText = null;
+        private int page = 1;
+        private readonly string outputFilename = "output.csv";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.webBrowser.Navigate("https://www.sgs.gov.cn/nameqry/searchTrdUseableName.action");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.searchText = this.cmbCriteria.Text;
+            this.page = 1;
+            //%CD%F8%C2%E7%BF%C6%BC%BC
+            var postData = $"query=query&zihao=&trdName={WebUtility.UrlEncode(this.searchText)}&f_type=%CD%F8%C2%E7%BF%C6%BC%BC&trdId=07&f_sort1=07++++";
+            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
+            byte[] bytes = encoding.GetBytes(postData);
+            string url = "https://www.sgs.gov.cn/nameqry/searchTrdUseableName.action";
+            webBrowser.Navigate(url, string.Empty, bytes, "Content-Type: application/x-www-form-urlencoded");
+        }
+
+        private async void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var text = this.webBrowser.Document.Body.InnerHtml;
+            //var pattern = @"<tr bgcolor=#FFF8E1 height=20>\s*<td class=p02>(?<name>.*)</td>\s*<td class=p02>(?<type>.*)</td>\s*<td class=p02>(?<category>.*)</td>\s*</tr>";
+            var pattern = @"<tr height=""20"" bgcolor=""#fff8e1"">\s*<td class=""p02"">(?<name>.*)</td>\s*<td class=""p02"">(?<type>.*)</td>\s*<td class=""p02"">(?<category>.*)</td>\s*</tr>";
+            var matches = Regex.Matches(text, pattern);
+            using (var file = File.Open(this.outputFilename, FileMode.Append, FileAccess.Write, FileShare.Write))
+            {
+                foreach (Match match in matches)
+                {
+                    var name = match.Groups["name"];
+                    var type = match.Groups["type"];
+                    var category = match.Groups["category"];
+                    var t = $"{name}, {type}, {category}" + Environment.NewLine;
+                    var b = Encoding.UTF8.GetBytes(t);
+                    file.Write(b, 0, b.Length);
+                }
+            }
+
+            await Task.Delay(1000);
+
+            page++;
+            this.webBrowser.Document.InvokeScript("formsub", new string[] { this.page.ToString() });
+            this.label1.Text = $"opening page {this.page}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var csv = new List<Hello>(); // or, List<YourClass>
+            var lines = System.IO.File.ReadAllLines(this.outputFilename);
+            foreach (string line in lines)
+                csv.Add(new Hello( line.Split(','))); // or, populate YourClass          
+            string json =  JsonConvert.SerializeObject(csv);
+            File.WriteAllText("test.json", json);
+        }
+
+        private class Hello
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public string Category { get; set; }
+            public Hello(params string[] values)
+            {
+                this.Name = values[0].Trim();
+                this.Type = values[1].Trim();
+                this.Category = values[2].Trim();
+            }
         }
     }
 }
